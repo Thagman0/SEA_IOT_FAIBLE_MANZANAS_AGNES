f0aec3ec0bb0c894b8dbc432710a342e794154e5
tytouan <agnes@et.eseia.fr>
2026-09-10 18:22:13 +0200

    1.4.2.2 SOS

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void setup() {
 }
 
 void loop() {
-
+    //1.3.2.2
     //Comment the println when terminal screenshot is not needed in order to keep temporal precision   
     //Serial.println("[Binome 01 - MANZANAS & AGNES] Hello Arduino World!");
 
@@ -42,9 +42,44 @@ void loop() {
     delay(2000);  // Wait 2 second
     */
     
-
+    //1.4.2.1
+    /*
     digitalWrite(2, HIGH);
     delay(50);  // N * 50 ms = 1 * 50 ms
     digitalWrite(2, LOW);
     delay(1950);  
+    */
+
+    //1.4.2.2
+    // In this case on morse unit would be 50 ms as described by 50 * N ms = 50 * 1 ms = 50 ms
+    //But this timing is to fast so we will mutliply it by 10 to be readable
+    // SOS = ... --- ...
+    int Morse_Unit = 500;
+    // S = 3 dots = 3 * 50 ms = 150 ms 
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(2, HIGH);
+        delay(Morse_Unit);  // N * 50 ms = 1 * 50 ms
+        digitalWrite(2, LOW);
+        if (i < 2)  // Avoid delay after the last dot
+            delay(Morse_Unit);
+    }
+    delay (3* Morse_Unit);  // Inter-letter space (3 units)
+    // O = 3 dashes = 3 * 150 ms = 450 ms
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(2, HIGH);
+        delay(3 * Morse_Unit);  // N * 50 ms = 3 * 50 ms
+        digitalWrite(2, LOW);
+        if (i < 2)  // Avoid delay after the last dash
+            delay(Morse_Unit);
+    }
+    delay (3 * Morse_Unit);  // Inter-letter space (3 units)
+    // S = 3 dots = 3 * 50 ms = 150 ms
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(2, HIGH);
+        delay(Morse_Unit);  // N * 50 ms = 1 * 50 ms
+        digitalWrite(2, LOW);
+        if (i < 2)  // Avoid delay after the last dot
+            delay(Morse_Unit);
+    }
+    delay (7 * Morse_Unit);  // Inter-Word space (7 units)
 }
